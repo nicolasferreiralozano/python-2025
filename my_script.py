f825a4d0356be8e14a1e66ea0e0a332a3072d3dd
nicolasferreiralozano <31458151+nicolasferreiralozano@users.noreply.github.com>
2025-01-27 10:58:56 -0800

    Update my_script.py

--- my_script.py
+++ my_script.py
@@ -3,11 +3,18 @@ from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 
 def load_cert(pem_cert: bytes):
-    r"""This explains how the function works, path "\\?\C:\hello\do".
+    """This explains how the function works, path "\\?\C:\hello\do".
     Is a path "\\?\"
     """
-    cert: x509.Certificate = x509.load_pem_x509_certificate(pem_cert, default_backend())
-    if datetime.now(timezone.utc) > cert.not_valid_after:
-        print("hello")
+    if pem_cert is None:
+        print("pem_cert is None")
     else:
-        print("goodbye")
+        cert: x509.Certificate = x509.load_pem_x509_certificate(pem_cert, default_backend())
+        if datetime.now(timezone.utc) > cert.not_valid_after:
+            print("hello")
+        else:
+            print("goodbye")
+
+if __name__ == "__main__":
+    print("hello world!")
+    load_cert(None)
